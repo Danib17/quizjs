01cce967451d7a7b1d65a8cbf0517bc44bf345f0
Zepold <dlopez0017@icloud.com>
2015-12-18 21:40:58 -0500

    added basic question answering fuctionality

--- quiz.js
+++ quiz.js
@@ -1,10 +1,5 @@
 //Collects how many problems the user is going to solve in the quiz
-var start = new Date().getTime() / 1000;
 var numOfProblems = Number(prompt('How many problems do you want to solve?'));
-var end = new Date().getTime() / 1000;
-
-var time = end - start
-console.log(`Took you ${time.toFixed(2)} seconds to enter a value`);
 
 //Forces user to enter a value greater than 0
 while (numOfProblems === 0) {
@@ -17,5 +12,15 @@ console.log(typeof numOfProblems);
 //Forloop will the quiz. Will run for how ever many problems the user entered
 //User can enter exit to finish the quiz early
 for(var x = 1; x <= numOfProblems; x++) {
-  console.log(x);
+  var numOne = Math.floor((Math.random() * 10) + 1);
+  var numTwo = Math.floor((Math.random() * 10) + 1);
+  var answer = numOne + numTwo;
+
+  var userAnswer = Number(prompt(`What is ${numOne} + ${numTwo}`))
+  if (userAnswer === answer) {
+    alert('Correct!');
+  }
+  else {
+    alert('Wrong!');
+  }
 }
