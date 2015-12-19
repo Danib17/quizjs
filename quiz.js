3b381a87c9f3c9c6ad1bc6cb8ad6e222b190603c
Zepold <dlopez0017@icloud.com>
2015-12-18 21:54:13 -0500

    put the quiz inside of a function named quiz, added a results function to give the results to the user, added number wrong and correct functionality for the results

--- quiz.js
+++ quiz.js
@@ -1,5 +1,6 @@
-//Collects how many problems the user is going to solve in the quiz
-var numOfProblems = Number(prompt('How many problems do you want to solve?'));
+var numberCorrect = 0 //Determines how many problems the user got correct
+var numberWrong = 0 //Determines how many the user got wrong
+var numOfProblems = Number(prompt('How many problems do you want to solve?')); //Collects how many problems the user is going to solve in the quiz
 
 //Forces user to enter a value greater than 0
 while (numOfProblems === 0) {
@@ -7,20 +8,33 @@ while (numOfProblems === 0) {
   numOfProblems = Number(prompt('Please enter how many problems do you want to solve?'));
 }
 
-console.log(typeof numOfProblems);
+quiz();
 
 //Forloop will the quiz. Will run for how ever many problems the user entered
 //User can enter exit to finish the quiz early
-for(var x = 1; x <= numOfProblems; x++) {
-  var numOne = Math.floor((Math.random() * 10) + 1);
-  var numTwo = Math.floor((Math.random() * 10) + 1);
-  var answer = numOne + numTwo;
+function quiz() {
+  for(var x = 1; x <= numOfProblems; x++) {
+    var numOne = Math.floor((Math.random() * 10) + 1);
+    var numTwo = Math.floor((Math.random() * 10) + 1);
+    var answer = numOne + numTwo;
 
-  var userAnswer = Number(prompt(`What is ${numOne} + ${numTwo}`))
-  if (userAnswer === answer) {
-    alert('Correct!');
+    var userAnswer = Number(prompt(`What is ${numOne} + ${numTwo}`))
+    if (userAnswer === answer) {
+      alert('Correct!');
+      numberCorrect++;
+    }
+    else {
+      alert('Wrong!');
+      numberWrong++;
+    }
   }
-  else {
-    alert('Wrong!');
-  }
+  results();
+}
+
+
+function results() {
+  alert('Results: \n' +
+        `Total problems: ${numOfProblems}\n` +
+        `Total correct: ${numberCorrect}\n` +
+        `Total wrong: ${numberWrong}\n`);
 }
